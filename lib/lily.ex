b0961155e5e540fcdaf5273919963390ec9c4556
Bill Allen <bill@k2bea.org>
2025-05-15 14:47:51 -0400

    add to lilypond writing, get octave correct for lilypond midi output

--- lib/lily.ex
+++ lib/lily.ex
@@ -13,18 +13,42 @@ defmodule Lily do
   @doc """
   Render a list of sonorities to a Lilypond string.
   """
-  @spec render([Sonority.t()]) :: String.t()
-  def render(sonorities) when is_list(sonorities) do
+  @spec render([Sonority.t()], keyword()) :: String.t()
+  def render(sonorities, opts \\ [])
+  def render(sonorities, opts) when is_list(sonorities) do
+    midi = Keyword.get(opts, :midi, false)
+    title = Keyword.get(opts, :title, "No Name")
+    midi_str = if midi, do: "\\midi { }", else: ""
     s = Lily.show(sonorities)
-    @preamble <> "{\n #{s} \n}"
+    @preamble <> "
+    \\book
+    {
+      \\header
+        {
+          title = \"#{title}\"
+        }
+      \\score
+        {
+          {
+          #{s}
+          }
+        \\layout { }
+        #{midi_str}
+      }
+    }"
   end
 
-  @spec render(Sonority.t()) :: String.t()
-  def render(sonority) do
+  @spec render(Sonority.t(), keyword()) :: String.t()
+  def render(sonority, _opts) do
     @preamble <> "{
       #{Sonority.show(sonority)}
     }"
   end
 
+  def write(sonorities, path, opts \\ []) do
+    out_path = Keyword.get(opts, :out_path, ".")
+    File.write(path, render(sonorities, opts))
+    System.cmd("lilypond", ["-s", "--output=#{Path.expand(out_path)}", path])
+  end
 
 end
